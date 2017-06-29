16d768545bc79d5a17537663be44188ce40ac449
Nasim Rahaman <nasim.rahaman@iwr.uni-heidelberg.de>
2017-06-29 17:30:53 +0200

    - transform to convert segmentation to membranes

---
+++ neurofire/transforms/segmentation.py
@@ -0,0 +1,22 @@
+import numpy as np
+from scipy.ndimage import convolve
+from inferno.io.transform.base import Transform
+
+
+class Segmentation2Membranes(Transform):
+    DTYPE_MAPPING = {'float32': 'float32',
+                     'float': 'float32',
+                     'double': 'float64',
+                     'float64': 'float64',
+                     'half': 'float16',
+                     'float16': 'float16'}
+
+    def __init__(self, dtype='float32', **super_kwargs):
+        super(Segmentation2Membranes, self).__init__(**super_kwargs)
+        assert dtype in self.DTYPE_MAPPING.keys()
+        self.dtype = self.DTYPE_MAPPING.get(dtype)
+
+    def image_funtion(self, image):
+        gx = convolve(np.float32(image), np.array([-1., 0., 1.]).reshape(1, 3))
+        gy = convolve(np.float32(image), np.array([-1., 0., 1.]).reshape(3, 1))
+        return getattr(np, self.dtype)((gx ** 2 + gy ** 2) > 0)
