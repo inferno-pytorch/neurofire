d3a9714525f785f21df10d7d62f17b8af56cf5d0
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2017-07-31 16:36:02 -0400

    Implementing torch function

--- neurofire/criteria/malis/README.md
+++ neurofire/criteria/malis/README.md
@@ -2,15 +2,21 @@
 
 ## Build
 
-This requires nifty, which can be included as submodule via:
+This requires nifty, which can be included as git submodule:
 
 ```
 $ cd malis_impl
 $ git submodule --init --recursive update
 ```
 
-TODO CMake
+Next, the malis C++ implementation needs to be build with make and cmake:
 
+```
+$ mkdir bld
+$ cd bld
+$ cmake ..
+$ make
+```
 
 ## Usage
 
--- neurofire/criteria/malis/malis.py
+++ neurofire/criteria/malis/malis.py
@@ -2,19 +2,63 @@ from torch.autograd import Function
 
 from malis_impl import malis_impl
 
+# malis loss and cpp impl adapted from:
+# https://github.com/naibaf7/caffe/blob/master/include/caffe/layers/malis_loss_layer.hpp
+# https://github.com/naibaf7/caffe/blob/master/src/caffe/layers/malis_loss_layer.cpp
 
-# Inherit from Function:
+# TODO: the caffee implementation parallelizes over the batches in the backward / forward pass
+# I have no idea how batches are handled in pytorch, so I haven't done any parallelization yet
+# However the gil for 'malis_impl' is lifted, so this CAN be used in multiple threads
+
+# TODO as far as I can tell, this is normal MALIS. I can't see, where in the caffe impl
+# constrained MALIS (aka MALA) comes into play
+
+# To create new loss, we inherit from Function:
 # https://github.com/pytorch/pytorch/blob/master/torch/autograd/function.py#L123
+# see also
+# http://pytorch.org/tutorials/beginner/examples_autograd/two_layer_net_custom_function.html
 class MalisLoss(Function):
     """
-    Malis loss
+    Malis Loss
     """
 
-    def __init__(self):
-        pass
+    def forward(self, affinities, groundtruth):
+        """
+        Apply malis forward pass to get the loss.
 
-    def forward(self):
-        pass
+        Parameters
+        ----------
+        affinities : torch.Variable wrapping the affinity tensor
+        groundtruth : torch.Variable wrapping the groundtruth tensor
 
-    def backward(self):
-        pass
+        Returns
+        -------
+        loss: malis loss
+        """
+
+        # fist, compute the positive loss and gradients
+        pos_gradients, pos_loss, _, _ = malis_impl(
+            affinities, groundtruth, True
+        )
+
+        # next, compute the negative loss and gradients
+        # TODO for constrained malis, we need to somehow restrict the segmentation here
+        neg_gradients, neg_loss, _, _ = malis_impl(
+            affinities, groundtruth, False
+        )
+
+        # save the combined gradient for the backward pass
+        self.save_for_backward(
+            -(neg_gradients + pos_gradients) / 2.
+        )
+
+        # return the combined loss
+        return (neg_loss + pos_loss) / 2.
+
+    def backward(self, grad_output):
+        """
+        Apply malis backward pass to get the gradients.
+        """
+
+        gradients, = self.saved_tensors
+        return gradients
