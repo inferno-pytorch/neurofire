8f802b871204a632c64216dd0b5aeb6ffc095a42
nrahaman <nasim.rahaman@iwr.uni-heidelberg.de>
2017-08-04 21:01:44 +0200

    - abc for malis - z-to-batch folding for constrained malis - more tests (failing)

--- tests/neurofire/criteria/malis/test_malis.py
+++ tests/neurofire/criteria/malis/test_malis.py
@@ -8,7 +8,7 @@ from torch.autograd import Variable
 class TestMalis(unittest.TestCase):
     BATCH_SIZE = 10
 
-    def generate_test_data(self, generate_2d=True):
+    def generate_test_data(self, generate_2d=True, reduce_num_affinity_channels=False):
         shape = (100, 100) if generate_2d else (100, 100, 100)
         dim = 2 if generate_2d else 3
 
@@ -26,6 +26,8 @@ class TestMalis(unittest.TestCase):
         affinities = np.array([affinities.copy() for _ in range(self.BATCH_SIZE)])
         # groundtruth needs an extra channel axis
         groundtruth = np.array([groundtruth.copy() for _ in range(self.BATCH_SIZE)])[:, None, ...]
+        if reduce_num_affinity_channels:
+            affinities = affinities[:, 1:, ...]
         return affinities, groundtruth
 
     def test_malis_loss(self):
@@ -41,10 +43,39 @@ class TestMalis(unittest.TestCase):
         # Validate
         self.assertIsNotNone(affinities.grad)
 
-    def test_constrained_malis_loss(self):
+    def test_malis_auto_loss_3d(self):
+        affinities, ground_truth = self.generate_test_data(generate_2d=False)
+        # Convert to variables
+        affinities = Variable(torch.from_numpy(affinities), requires_grad=True)
+        ground_truth = Variable(torch.from_numpy(ground_truth))
+        # Build criterion
+        criterion = MalisLoss(malis_dim='auto')
+        # Evaluate criterion
+        loss = criterion(affinities, ground_truth).sum()
+        loss.backward()
+        # Validate
+        self.assertIsNotNone(affinities.grad)
+        self.assertEqual(affinities.grad.size(1), 3)
+        self.assertEqual(affinities.grad.dim(), 5)
+
+    def test_malis_2d_loss_3d(self):
+        affinities, ground_truth = self.generate_test_data(generate_2d=False,
+                                                           reduce_num_affinity_channels=True)
+        # Convert to variables
+        affinities = Variable(torch.from_numpy(affinities), requires_grad=True)
+        ground_truth = Variable(torch.from_numpy(ground_truth))
+        # Build criterion
+        criterion = MalisLoss(malis_dim=2)
+        # Evaluate criterion
+        loss = criterion(affinities, ground_truth).sum()
+        loss.backward()
+        # Validate
+        self.assertIsNotNone(affinities.grad)
+        self.assertEqual(affinities.grad.size(1), 2)
+        self.assertEqual(affinities.grad.dim(), 5)
+
+    def test_constrained_malis_auto_loss_2d(self):
         affinities, ground_truth = self.generate_test_data()
-        affinities = np.array([affinities.copy() for _ in range(self.BATCH_SIZE)])
-        ground_truth = np.array([ground_truth.copy() for _ in range(self.BATCH_SIZE)])
         # Convert to variables
         affinities = Variable(torch.from_numpy(affinities), requires_grad=True)
         ground_truth = Variable(torch.from_numpy(ground_truth))
@@ -56,10 +87,39 @@ class TestMalis(unittest.TestCase):
         # Validate
         self.assertIsNotNone(affinities.grad)
 
+    def test_constrained_malis_auto_loss_3d(self):
+        affinities, ground_truth = self.generate_test_data(generate_2d=False)
+        # Convert to variables
+        affinities = Variable(torch.from_numpy(affinities), requires_grad=True)
+        ground_truth = Variable(torch.from_numpy(ground_truth))
+        # Build criterion
+        criterion = ConstrainedMalisLoss()
+        # Evaluate criterion
+        loss = criterion(affinities, ground_truth).sum()
+        loss.backward()
+        # Validate
+        self.assertIsNotNone(affinities.grad)
+        self.assertEqual(affinities.grad.size(1), 3)
+        self.assertEqual(affinities.grad.dim(), 5)
+
+    def test_constrained_malis_2d_loss_3d(self):
+        affinities, ground_truth = self.generate_test_data(generate_2d=False,
+                                                           reduce_num_affinity_channels=True)
+        # Convert to variables
+        affinities = Variable(torch.from_numpy(affinities), requires_grad=True)
+        ground_truth = Variable(torch.from_numpy(ground_truth))
+        # Build criterion
+        criterion = ConstrainedMalisLoss()
+        # Evaluate criterion
+        loss = criterion(affinities, ground_truth).sum()
+        loss.backward()
+        # Validate
+        self.assertIsNotNone(affinities.grad)
+        self.assertEqual(affinities.grad.size(1), 2)
+        self.assertEqual(affinities.grad.dim(), 5)
+
     def test_malis(self):
         affinities, ground_truth = self.generate_test_data()
-        affinities = np.array([affinities.copy() for _ in range(self.BATCH_SIZE)])
-        ground_truth = np.array([ground_truth.copy() for _ in range(self.BATCH_SIZE)])
         # Convert to variables
         affinities = Variable(torch.from_numpy(affinities), requires_grad=True)
         ground_truth = Variable(torch.from_numpy(ground_truth))
@@ -75,9 +135,6 @@ class TestMalis(unittest.TestCase):
         if not torch.cuda.is_available():
             return
         affinities, ground_truth = self.generate_test_data()
-        affinities = np.array([affinities.copy() for _ in range(self.BATCH_SIZE)])
-        # 4D ground truth, as one would have from the feeder
-        ground_truth = np.array([ground_truth.copy() for _ in range(self.BATCH_SIZE)])[:, None, ...]
         # Convert to variables and transfer to GPU
         affinities = Variable(torch.from_numpy(affinities).cuda(), requires_grad=True)
         ground_truth = Variable(torch.from_numpy(ground_truth).cuda())
@@ -93,5 +150,5 @@ class TestMalis(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    TestMalis().test_malis_loss()
+    TestMalis().test_constrained_malis_auto_loss_2d()
     # unittest.main()
