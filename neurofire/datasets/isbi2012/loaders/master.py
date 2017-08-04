dcc7974e3275733ef311ae850a2eb4ecad5d324a
Constantin Pape <c.pape@gmx.net>
2017-08-03 22:18:44 -0400

    Isbi hdf5 loader

--- neurofire/datasets/isbi2012/loaders/master.py
+++ neurofire/datasets/isbi2012/loaders/master.py
@@ -7,8 +7,8 @@ from inferno.utils.io_utils import yaml2dict
 
 from torch.utils.data.dataloader import DataLoader
 
-from .membranes import MembraneVolume
-from .raw import RawVolume
+from .membranes import MembraneVolume, MembraneVolumeHDF5, AffinityVolumeHDF5
+from .raw import RawVolume, RawVolumeHDF5
 
 
 class ISBI2012Dataset(Zip):
@@ -45,7 +45,47 @@ class ISBI2012Dataset(Zip):
         return cls(volume_config=volume_config, slicing_config=slicing_config)
 
 
-def get_isbi_loader(config):
+class ISBI2012DatasetHDF5(Zip):
+    def __init__(self, volume_config, slicing_config):
+        assert isinstance(volume_config, dict)
+        assert isinstance(slicing_config, dict)
+        assert 'raw' in volume_config
+        assert ('membranes' in volume_config) != ('affinities' in volume_config)
+        # Get kwargs for raw volume
+        raw_volume_kwargs = dict(volume_config.get('raw'))
+        raw_volume_kwargs.update(slicing_config)
+        # Build raw volume
+        self.raw_volume = RawVolumeHDF5(**raw_volume_kwargs)
+        # Build membrane or affinity volume
+        if 'membranes' in volume_config:
+            membrane_volume_kwargs = dict(volume_config.get('membranes'))
+            membrane_volume_kwargs.update(slicing_config)
+            self.membrane_or_affinity_volume = MembraneVolumeHDF5(**membrane_volume_kwargs)
+        elif 'affinities' in volume_config:
+            affinity_volume_kwargs = dict(volume_config.get('affinities'))
+            affinity_volume_kwargs.update(slicing_config)
+            self.membrane_or_affinity_volume = AffinityVolumeHDF5(**affinity_volume_kwargs)
+        # Initialize zip
+        super(ISBI2012DatasetHDF5, self).__init__(self.raw_volume, self.membrane_or_affinity_volume, sync=True)
+        # Get transforms
+        self.transforms = self.get_transforms()
+
+    def get_transforms(self):
+        transforms = Compose(RandomFlip3D(),
+                             RandomRotate(),
+                             ElasticTransform(alpha=2000., sigma=50.),  # Hard coded for now
+                             AsTorchBatch(2))
+        return transforms
+
+    @classmethod
+    def from_config(cls, config):
+        config = yaml2dict(config)
+        volume_config = config.get('volume_config')
+        slicing_config = config.get('slicing_config')
+        return cls(volume_config=volume_config, slicing_config=slicing_config)
+
+
+def get_isbi_loader(config, use_hdf5=False):
     """
     Gets ISBI2012 Loader given a the path to a configuration file.
 
@@ -53,6 +93,8 @@ def get_isbi_loader(config):
     ----------
     config : str or dict
         (Path to) Data configuration.
+    use_hdf5: bool
+        flag to specify whether data is hdf5 or tiff
 
     Returns
     -------
@@ -60,6 +102,9 @@ def get_isbi_loader(config):
         Data loader built as configured.
     """
     config = yaml2dict(config)
-    dataset = ISBI2012Dataset.from_config(config)
+    if use_hdf5:
+        dataset = ISBI2012DatasetHDF5.from_config(config)
+    else:
+        dataset = ISBI2012Dataset.from_config(config)
     loader = DataLoader(dataset, **config.get('loader_config'))
     return loader
--- neurofire/datasets/isbi2012/loaders/membranes.py
+++ neurofire/datasets/isbi2012/loaders/membranes.py
@@ -1,7 +1,8 @@
-from inferno.io.volumetric import TIFVolumeLoader
+from inferno.io.volumetric import TIFVolumeLoader, HDF5VolumeLoader
 from inferno.io.transform import Compose
 from inferno.io.transform.generic import Cast
 from ....transforms.segmentation import NegativeExponentialDistanceTransform
+from ....transforms.segmentation import Segmentation2Affinities
 
 
 class MembraneVolume(TIFVolumeLoader):
@@ -22,4 +23,44 @@ class MembraneVolume(TIFVolumeLoader):
             NegativeExponentialDistanceTransform(gain=self.nedt_gain, invert=False),
             Cast(self.dtype)
         )
+        return transforms
+
+
+class MembraneVolumeHDF5(HDF5VolumeLoader):
+    def __init__(self, path, path_in_h5_dataset, dtype='float32', nedt_gain=None, **slicing_config):
+        super(MembraneVolumeHDF5, self).__init__(path=path, path_in_h5_dataset=path_in_h5_dataset, **slicing_config)
+        assert isinstance(dtype, str)
+        assert isinstance(nedt_gain, (type(None), int, float))
+        self.dtype = dtype
+        self.nedt_gain = nedt_gain or 1.
+        # Make transforms
+        self.transforms = self.get_transforms()
+
+    def get_transforms(self):
+        transforms = Compose(
+            NegativeExponentialDistanceTransform(gain=self.nedt_gain, invert=False),
+            Cast(self.dtype)
+        )
+        return transforms
+
+
+# TODO make segmentation for malis accessible ?!
+class AffinityVolumeHDF5(MembraneVolumeHDF5):
+    def __init__(self, path, path_in_h5_dataset=None, data_slice=None, name=None,
+                 dtype='float32', affinity_dim=3, **slicing_config):
+        # Set attributes
+        self.affinity_dim = affinity_dim
+        # Initialize super
+        super(AffinityVolumeHDF5, self).__init__(path, path_in_h5_dataset=path_in_h5_dataset,
+                                                 data_slice=data_slice, name=name,
+                                                 dtype=dtype, nedt_gain=None,
+                                                 **slicing_config)
+        # We don't need a nedt_gain anymore
+        del self.nedt_gain
+
+    def get_transforms(self):
+        # The Segmentation2Affinities adds a channel dimension
+        transforms = Compose(Segmentation2Affinities(self.affinity_dim,
+                                                     add_singleton_channel_dimension=True),
+                             Cast(self.dtype))
         return transforms
