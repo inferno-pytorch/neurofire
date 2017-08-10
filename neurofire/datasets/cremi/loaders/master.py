3470df6c00f8d695f72390be437adee09a1b3252
nrahaman <nasim.rahaman@iwr.uni-heidelberg.de>
2017-08-11 00:22:40 +0200

    - master CREMI feeder can now be configured (e.g. with parameters for elastic trafo, etc.) - updated test config file

--- neurofire/datasets/cremi/loaders/master.py
+++ neurofire/datasets/cremi/loaders/master.py
@@ -2,7 +2,7 @@ from inferno.io.core import Zip, Concatenate
 from inferno.io.transform import Compose
 from inferno.io.transform.generic import AsTorchBatch
 from inferno.io.transform.volume import RandomFlip3D
-from inferno.io.transform.image import RandomRotate, ElasticTransform
+from inferno.io.transform.image import RandomRotate, ElasticTransform, CenterCrop
 from inferno.utils.io_utils import yaml2dict
 
 from torch.utils.data.dataloader import DataLoader
@@ -12,7 +12,7 @@ from .raw import RawVolume
 
 
 class CREMIDataset(Zip):
-    def __init__(self, name, volume_config, slicing_config):
+    def __init__(self, name, volume_config, slicing_config, master_config=None):
         assert isinstance(volume_config, dict)
         assert isinstance(slicing_config, dict)
         assert 'raw' in volume_config
@@ -39,23 +39,39 @@ class CREMIDataset(Zip):
         # Initialize zip
         super(CREMIDataset, self).__init__(self.raw_volume, self.membrane_or_affinity_volume,
                                            sync=True)
+        # Set master config (for transforms)
+        self.master_config = {} if master_config is None else master_config
         # Get transforms
         self.transforms = self.get_transforms()
 
     def get_transforms(self):
         transforms = Compose(RandomFlip3D(),
-                             RandomRotate(),
-                             # Hard coded for now:
-                             ElasticTransform(alpha=2000., sigma=50.))
+                             RandomRotate())
+        if 'elastic_transform' in self.master_config:
+            # Elastic transforms can be skipped by setting elastic_transform to false in the
+            # yaml config file.
+            if self.master_config.get('elastic_transform'):
+                elastic_transform_config = self.master_config.get('elastic_transform')
+                transforms.add(ElasticTransform(alpha=elastic_transform_config.get('alpha', 2000.),
+                                                sigma=elastic_transform_config.get('sigma', 50.),
+                                                order=elastic_transform_config.get('order', 1)))
+        else:
+            # Preserve legacy behaviour
+            transforms.add(ElasticTransform(alpha=2000., sigma=50.))
+        if self.master_config.get('crop_after_elastic_transform', False):
+            # One might need to crop after elastic transform to avoid edge artefacts of affinity
+            # computation being warped into the FOV.
+            transforms.add(CenterCrop(**self.master_config.get('crop_after_elastic_transform', {})))
         return transforms
 
 
 class CREMIDatasets(Concatenate):
-    def __init__(self, names, volume_config, slicing_config):
+    def __init__(self, names, volume_config, slicing_config, master_config=None):
         # Make datasets and concatenate
         datasets = [CREMIDataset(name=name,
                                  volume_config=volume_config,
-                                 slicing_config=slicing_config)
+                                 slicing_config=slicing_config,
+                                 master_config=master_config)
                     for name in names]
         # Concatenate
         super(CREMIDatasets, self).__init__(*datasets)
@@ -71,8 +87,9 @@ class CREMIDatasets(Concatenate):
         names = config.get('dataset_names')
         volume_config = config.get('volume_config')
         slicing_config = config.get('slicing_config')
+        master_config = config.get('master_config')
         return cls(names=names, volume_config=volume_config,
-                   slicing_config=slicing_config)
+                   slicing_config=slicing_config, master_config=master_config)
 
 
 def get_cremi_loaders(config):
