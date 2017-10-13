730d2dc58265dfaa5af4abb2ecc8ac7214fa8b6e
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2017-10-12 21:26:14 -0400

    Starting volume tests

---
+++ tests/neurofire/transforms/volume.py
@@ -0,0 +1,11 @@
+# import unittest
+import neurofire.transforms.volume as vol
+import numpy as np
+import h5py
+# import torch
+
+
+def view_random_slide():
+    from volumina_viewer import volumina_n_layer
+    path = vigra.readHDF5(
+    )
