a61831e56ab05b8356ff4a9eebd76ef3e1c82fa9
Constantin Pape <c.pape@gmx.net>
2017-12-20 23:40:41 +0100

    Mock up for loss transformations

---
+++ neurofire/criteria/loss_transformations.py
@@ -0,0 +1,13 @@
+from inferno.transforms import Transform
+
+# TODO
+# Implement loss transforms
+# (prediction, target) -> (transformed_prediction, transformed_target)
+# Implement by subclassing transform and providing batch or tensor function
+# TODO how do we keep trafos for prediction / target optional ?
+
+
+# TODO implement all the different masking functions
+
+# TODO provide functionality to do trafos on gpu ?!?
+# (for affinity trafos on gpu)
--- neurofire/criteria/loss_wrapper.py
+++ neurofire/criteria/loss_wrapper.py
@@ -18,9 +18,16 @@ class LossWrapper(nn.Module):
 
     def forward(self, prediction, target):
         # we apply the transformations to the prediction
-        # TODO in https://github.com/nasimrahaman/neuro-skunkworks/blob/more-cremi/skunkworks/datasets/cremi/criteria/euclidean.py#L36
-        # we make a new variable `masked_prediction`. Is there a special reason for it (no gradients for inplace operations)
+
+        # TODO in
+        # https://github.com/nasimrahaman/neuro-skunkworks/blob/more-cremi/skunkworks/datasets/cremi/criteria/euclidean.py#L36
+        # we make a new variable `masked_prediction`.
+        # Is there a special reason for it (no gradients for inplace operations)
         # or could we also write `prediction = prediction`?
+
+        # TODO
+        # we also want an option to transform the target
+
         if self.transforms is None:
             transformed_prediction = self.transforms(prediction, target)
         else:
@@ -28,14 +35,6 @@ class LossWrapper(nn.Module):
 
         loss = self.criterion(transformed_prediction, target)
         return loss
-
-
-# TODO We need to have `loss transforms` analogous to the inferno `transforms`,
-# but which take `prediction` and `target` as arguments and return
-# `transformed_prediction`
-# I don't really know how to best implement this.
-# Should we subclass inferno transforms or should we make a new
-# base class for this
 
 
 # TODO something analogous to `AsSegmentationCriterion` from neuro-skunkworks to
