7e16ba5e97e710e7882499e60e92d22d39952796
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2018-07-02 21:13:49 +0200

    Update affinity tests

--- tests/neurofire/transform/test_segmentation.py
+++ tests/neurofire/transform/test_segmentation.py
@@ -1,6 +1,5 @@
 import unittest
 import numpy as np
-import torch
 
 
 class TestSegmentation(unittest.TestCase):
@@ -78,106 +77,68 @@ class TestSegmentation(unittest.TestCase):
         self.assertTrue(np.allclose(output, expected))
 
     def test_affs_toy(self):
-        from neurofire.transform.segmentation import Segmentation2Affinities
+        from neurofire.transform.affinities import Segmentation2Affinities
         segmentation, expected = self.generate_toy_data()
-        transform = Segmentation2Affinities(dim=2)
-        output = transform(segmentation[None, :]).squeeze()
+        offsets = [[-1, 0], [0, -1]]
+        transform = Segmentation2Affinities(offsets=offsets)
+        output = transform(segmentation)
+        # print(output)
+        # print()
+        # print(expected)
         self.assertEqual(output.shape, expected.shape)
         self.assertTrue(np.allclose(output, expected))
 
     def test_affs_random(self):
-        from neurofire.transform.segmentation import Segmentation2Affinities
-
-        # 3D with 3D affinities
-        wannabe_groundtruth = np.random.uniform(size=(1, 16, 512, 512))
-        # Build transform
-        transform = Segmentation2Affinities(dim=3)
-        output = transform(wannabe_groundtruth)
-        self.assertSequenceEqual((16, 512, 512), output.shape[1:])
-        self.assertEqual(output.shape[0], 3)
-
-        # 3D with 2D affinities
-        wannabe_groundtruth = np.random.uniform(size=(1, 16, 512, 512))
-        # Build transform
-        transform = Segmentation2Affinities(dim=2)
-        output = transform(wannabe_groundtruth)
-        self.assertSequenceEqual((16, 512, 512), output.shape[1:])
-        self.assertEqual(output.shape[0], 2)
+        from neurofire.transform.affinities import Segmentation2Affinities
+        offsets_2d = [[-1, 0], [0, -1]]
+        offsets_3d = [[-1, 0, 0], [0, -1, 0], [0, 0, -1]]
 
         # 2D with 2D affinities
-        wannabe_groundtruth = np.random.uniform(size=(1, 512, 512))
+        wannabe_groundtruth = np.random.uniform(size=(512, 512))
         # Build transform
-        transform = Segmentation2Affinities(dim=2)
+        transform = Segmentation2Affinities(offsets=offsets_2d)
         output = transform(wannabe_groundtruth)
         self.assertSequenceEqual((512, 512), output.shape[1:])
         self.assertEqual(output.shape[0], 2)
 
-    # FIXME outputs do not agree 100 %
-    def test_affs_2d_orders(self):
-        from neurofire.transform.segmentation import Segmentation2Affinities
-        shape = (1, 64, 64)
-        segmentation = self.generate_segmentation(shape)
+        # 3D with 3D affinities
+        wannabe_groundtruth = np.random.uniform(size=(16, 512, 512))
+        # Build transform
+        transform = Segmentation2Affinities(offsets=offsets_3d)
+        output = transform(wannabe_groundtruth)
+        self.assertSequenceEqual((16, 512, 512), output.shape[1:])
+        self.assertEqual(output.shape[0], 3)
 
-        def order_to_offsets(order):
-            return [(-order, 0), (0, -order)]
-
-        for order in (1, 2, 3, 5, 7, 20):
-            print("2D Test: Checking order", order)
-            # output from the segmentation module
-            transform = Segmentation2Affinities(dim=2, order=order)
-            output = transform(segmentation).squeeze()
-
-            output_expected = self.affinities_brute_force(segmentation.squeeze(),
-                                                          order_to_offsets(order))
-
-            self.assertEqual(output.shape, output_expected.shape)
-            print(output.shape)
-            print(np.sum(np.isclose(output, output_expected)), '/', output.size)
-            where = np.where(np.logical_not(np.isclose(output, output_expected)))
-            print(where)
-            print(output[where], output_expected[where])
-            self.assertTrue(np.allclose(output, output_expected))
-
-    # FIXME outputs do not agree 100 %
-    def test_affs_3d_orders(self):
-        from neurofire.transform.segmentation import Segmentation2Affinities
-        shape = (1, 64, 64, 64)
-        segmentation = self.generate_segmentation(shape)
-
-        def order_to_offsets(order):
-            return [(-order, 0, 0), (0, -order, 0), (0, 0, -order)]
-
-        for order in (1, 2, 3, 5, 7, 20):
-            print("3D Test: Checking order", order)
-            # output from the segmentation module
-            transform = Segmentation2Affinities(dim=3, order=order)
-            output = transform(segmentation).squeeze()
-
-            output_expected = self.affinities_brute_force(segmentation.squeeze(),
-                                                          order_to_offsets(order))
-
-            self.assertEqual(output.shape, output_expected.shape)
-            print(np.sum(np.isclose(output, output_expected)), '/', output.size)
-            self.assertTrue(np.allclose(output, output_expected))
-
-    # FIXME outputs do not agree 100 %
-    # TODO test for 2D
-    def test_affs_from_offsets_3D(self):
-        from neurofire.transform.segmentation import Segmentation2AffinitiesFromOffsets
-        shape = (1, 64, 64, 64)
+    def test_affs_from_offsets_2d(self):
+        from neurofire.transform.affinities import Segmentation2Affinities
+        shape = (64, 64)
         segmentation = self.generate_segmentation(shape).astype('int32')
-        seg_torch = torch.from_numpy(segmentation)
+
+        offsets = [(1, 0), (0, 1),
+                   (3, 0), (0, 3),
+                   (1, 1), (-1, 1)]
+
+        trafo = Segmentation2Affinities(offsets=offsets)
+        output = trafo(segmentation)
+        output_expected = self.affinities_brute_force(segmentation.squeeze(), offsets)
+        self.assertEqual(output.shape, output_expected.shape)
+        # print(np.sum(np.isclose(output, output_expected)), '/', output.size)
+        self.assertTrue(np.allclose(output, output_expected))
+
+    def test_affs_from_offsets_3d(self):
+        from neurofire.transform.affinities import Segmentation2Affinities
+        shape = (64, 64, 64)
+        segmentation = self.generate_segmentation(shape).astype('int32')
 
         offsets = [(1, 0, 0), (0, 1, 0), (0, 0, 1),
                    (3, 0, 0), (0, 3, 0), (0, 0, 3),
                    (1, 1, 0), (-1, 1, 0)]
 
-        trafo = Segmentation2AffinitiesFromOffsets(3, offsets,
-                                                   add_singleton_channel_dimension=False)
-        output = trafo(seg_torch).numpy()
+        trafo = Segmentation2Affinities(offsets=offsets)
+        output = trafo(segmentation)
         output_expected = self.affinities_brute_force(segmentation.squeeze(), offsets)
         self.assertEqual(output.shape, output_expected.shape)
-        print(np.sum(np.isclose(output, output_expected)), '/', output.size)
+        # print(np.sum(np.isclose(output, output_expected)), '/', output.size)
         self.assertTrue(np.allclose(output, output_expected))
 
     def test_cc_2d(self):
