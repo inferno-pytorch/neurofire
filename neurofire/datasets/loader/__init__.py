bf2705c0dd30832ab8c56b63a8e78b460c13ceb2
Constantin Pape <c.pape@gmx.net>
2018-12-26 14:52:34 +0100

    Update loader init

--- neurofire/datasets/loader/__init__.py
+++ neurofire/datasets/loader/__init__.py
@@ -1,2 +1,2 @@
-from .raw import RawVolume, MultiscaleRawVolume
+from .raw import RawVolume, MultiscaleRawVolume, RawVolumeWithDefectAugmentation
 from .segmentation import SegmentationVolume, N5SegmentationVolume
