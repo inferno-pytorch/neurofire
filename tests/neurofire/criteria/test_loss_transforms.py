1c184728cc358d677d18e169e531bb1672483662
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2019-01-10 16:31:49 +0100

    Update masking tests

--- tests/neurofire/criteria/test_loss_transforms.py
+++ tests/neurofire/criteria/test_loss_transforms.py
@@ -1,10 +1,12 @@
 import unittest
 import numpy as np
 import torch
-from torch.autograd import Variable
 
 from inferno.extensions.criteria.set_similarity_measures import SorensenDiceLoss
-from .base_test import BaseTest
+try:
+    from .base_test import BaseTest
+except ImportError:
+    from base_test import BaseTest
 
 try:
     import affogato
@@ -18,25 +20,23 @@ class TestLossTransforms(BaseTest):
     def test_mask_ignore_label(self):
         from neurofire.criteria.loss_transforms import MaskIgnoreLabel
         from neurofire.transform.segmentation import Segmentation2Membranes
-        trafo = MaskIgnoreLabel(0)
+        trafo = MaskIgnoreLabel(-1)
         seg_trafo = Segmentation2Membranes()
 
         seg = self.make_segmentation_with_ignore(self.shape)
         ignore_mask = seg == 0
-        seg_var = Variable(torch.from_numpy(seg.astype('float32')))
 
         # make membrane target
-        target = torch.from_numpy(seg_trafo(seg))
-        target = Variable(target, requires_grad=False)
-        self.assertEqual(target.size(), seg_var.size())
+        target = seg_trafo(seg)
+        target[ignore_mask] = -1
+        target = torch.from_numpy(target)
+        target.requires_grad = False
+        self.assertEqual(target.shape, seg.shape)
 
         # make dummy torch prediction
-        prediction = Variable(torch.Tensor(*self.shape).uniform_(0, 1), requires_grad=True)
-        # NOTE Can't check that target is not altered here, because we give the segmentation and not
-        # the target to the transformation
-        # We could check the segmentation, but in the long run, we wan't something like
-        # retain_segmentation also for `Segmentation2Membranes`
-        masked_prediction, _ = trafo(prediction, seg_var)
+        prediction = torch.Tensor(*self.shape).uniform_(0, 1)
+        prediction.requires_grad = True
+        masked_prediction, _ = trafo(prediction, target)
 
         self.assertEqual(prediction.size(), masked_prediction.size())
 
@@ -52,26 +52,28 @@ class TestLossTransforms(BaseTest):
         self.assertTrue((grads[ignore_mask] == 0).all())
         self.assertTrue((grads[np.logical_not(ignore_mask)] != 0).all())
 
-    @unittest.skipUnless(WITH_AFF, "need affogato")
+    # @unittest.skipUnless(WITH_AFF, "need affogato")
+    @unittest.skip
     def test_transition_mask(self):
         from neurofire.criteria.loss_transforms import MaskTransitionToIgnoreLabel
-        from neurofire.transform.segmentation import Segmentation2Affinities
+        from neurofire.transform.affinities import Segmentation2Affinities
 
         offsets = [(1, 0, 0), (0, 1, 0), (0, 0, 1),
                    (9, 0, 0), (0, 9, 0), (0, 0, 9),
                    (9, 4, 0), (4, 9, 0), (9, 0, 9)]
         trafo = MaskTransitionToIgnoreLabel(offsets, ignore_label=0)
-        aff_trafo = Segmentation2Affinities(offsets, retain_segmentation=True,
-                                            retain_mask=True)
+        aff_trafo = Segmentation2Affinities(offsets=offsets, retain_segmentation=True,
+                                            retain_mask=False)
 
         seg = self.make_segmentation_with_ignore(self.shape)
         ignore_mask = self.brute_force_transition_masking(seg, offsets)
-        target = Variable(torch.Tensor(aff_trafo(seg.astype('float32'))[None]),
-                          requires_grad=False)
+        target = torch.from_numpy(aff_trafo(seg.astype('float32'))[None])
+        target.requires_grad = False
 
         tshape = target.size()
         pshape = (tshape[0], tshape[1] - 1) + tshape[2:]
-        prediction = Variable(torch.Tensor(*pshape).uniform_(0, 1), requires_grad=True)
+        prediction = torch.Tensor(*pshape).uniform_(0, 1)
+        prediction.require_grad = True
         masked_prediction, target_ = trafo(prediction, target)
 
         self.assertEqual(masked_prediction.size(), prediction.size())
