38fee55869ff732844c5661c7ac04b0792436b0d
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2019-04-15 16:12:48 +0200

    Make connected components in segmentation volume optional

--- neurofire/datasets/loader/segmentation.py
+++ neurofire/datasets/loader/segmentation.py
@@ -8,8 +8,9 @@ from neurofire.transform.segmentation import ConnectedComponents3D
 class SegmentationVolume(io.HDF5VolumeLoader):
     def __init__(self, path, path_in_file,
                  data_slice=None, name=None, dtype='float32',
-                 **slicing_config):
+                 label_volume=True, **slicing_config):
         # Init super
+        self.label_volume = label_volume
         super(SegmentationVolume, self).__init__(path=path, path_in_h5_dataset=path_in_file,
                                                  data_slice=data_slice, name=name, **slicing_config)
 
@@ -19,25 +20,32 @@ class SegmentationVolume(io.HDF5VolumeLoader):
         self.transforms = self.get_transforms()
 
     def get_transforms(self):
-        transforms = Compose(ConnectedComponents3D(),
-                             Cast(self.dtype))
+        if self.label_volume:
+            transforms = Compose(ConnectedComponents3D(),
+                                 Cast(self.dtype))
+        else:
+            transforms = Cast(self.dtype)
         return transforms
 
 
 class N5SegmentationVolume(io.LazyN5VolumeLoader):
     def __init__(self, path, path_in_file,
                  data_slice=None, name=None, dtype='float32',
-                 **slicing_config):
+                 label_volume=True, **slicing_config):
         # Init super
         super(N5SegmentationVolume, self).__init__(path=path, path_in_file=path_in_file,
                                                    data_slice=data_slice, name=name, **slicing_config)
 
         assert isinstance(dtype, str)
+        self.label_volume = label_volume
         self.dtype = dtype
         # Make transforms
         self.transforms = self.get_transforms()
 
     def get_transforms(self):
-        transforms = Compose(ConnectedComponents3D(),
-                             Cast(self.dtype))
+        if self.label_volume:
+            transforms = Compose(ConnectedComponents3D(),
+                                 Cast(self.dtype))
+        else:
+            transforms = Cast(self.dtype)
         return transforms
